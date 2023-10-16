fa752e02cac2e7c37aeead5d072dc313079dc84a
Chris <102271783+roninchris@users.noreply.github.com>
2023-10-15 21:08:15 -0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,31 +1,21 @@
 public class Node {
-
     Node esquerda;
     int info;
     Node direita;
+    Node raiz;
 
     public Node getEsquerda() {
         return esquerda;
-    }
-
-    public void setEsquerda(Node esquerda) {
-        this.esquerda = esquerda;
     }
 
     public int getInfo() {
         return info;
     }
 
-    public void setInfo(int info) {
-        this.info = info;
-    }
-
     public Node getDireita() {
         return direita;
     }
 
-    public void setDireita(Node direita) {
-        this.direita = direita;
-    }
-
 }
+
+
