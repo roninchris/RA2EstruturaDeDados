3f052aac01271c1c43de749479492947a712c829
Chris <102271783+roninchris@users.noreply.github.com>
2023-10-15 21:08:03 -0300

    Update arvore.java

--- arvore.java
+++ arvore.java
@@ -1,5 +1,3 @@
-
-
 public class arvore {
 
     Node raiz;
@@ -16,105 +14,90 @@ public class arvore {
 
         if (raiz == null) {
             raiz = novo;
-            return;
-        }
-
-        Node atual = raiz;
-        Node anterior;
-        while (true) {
-            anterior = atual;
-            if (valor <= atual.info) {
-                if (atual.esquerda == null) {
-                    anterior.esquerda = novo;
-                    return;
+            System.out.println("Raiz adicionada de valor: " + novo.getInfo());
+        } else {
+            Node atual = raiz;
+            Node anterior;
+            while (true) {
+                anterior = atual;
+                if (valor <= atual.info) {
+                    atual = atual.esquerda;
+                    if (atual == null) {
+                        System.out.println("O pai desta folha eh: " + anterior.getInfo());
+                        anterior.esquerda = novo;
+                        System.out.println("O valor adicionado na esquerda foi: " + anterior.esquerda.getInfo());
+                        return;
+                    }
+                } else {
+                    atual = atual.direita;
+                    if (atual == null) {
+                        System.out.println("O pai desta folha eh: " + anterior.getInfo());
+                        anterior.direita = novo;
+                        System.out.println("O valor adicionado na direita foi: " + anterior.direita.getInfo());
+                        return;
+                    }
                 }
-                atual = atual.esquerda;
-            } else {
-                if (atual.direita == null) {
-                    anterior.direita = novo;
-                    return;
-                }
-                atual = atual.direita;
             }
         }
     }
 
-    public Node buscar(int elemento) {
-        Node atual = raiz;
-        List<Node> caminho = new ArrayList<>();
-        while (atual != null && atual.info != elemento) {
-            caminho.add(atual);
-            if (atual.info > elemento)
-                atual = atual.esquerda;
-            else
-                atual = atual.direita;
+    public void emOrdem(Node atual) {
+        if (atual != null) {
+            emOrdem(atual.esquerda);
+            System.out.println(atual.info + " ");
+            emOrdem(atual.direita);
         }
-        if (atual == null)
-            return null;
-        return atual;
     }
 
-    public static int Altura(Node no) {
-        if (no == null) {
+    public void preOrdem(Node atual) {
+        if(atual != null) {
+            System.out.println(atual.info + " ");
+            preOrdem(atual.esquerda);
+            preOrdem(atual.direita);
+        }
+    }
+
+    public void posOrdem(Node atual) {
+        if(atual != null) {
+            posOrdem(atual.esquerda);
+            posOrdem(atual.direita);
+            System.out.println(atual.info + " ");
+        }
+    }
+    public Node buscar(int elemento){
+        Node atual = raiz;
+        while(atual != null && atual.getInfo()!=elemento){
+            if(atual.getInfo()>elemento)
+                atual = atual.getEsquerda;
+            else
+                atual = atual.getDireita;
+        }
+        return null;    
+    }
+    public static int Altura(Node no){
+        if (no == null){
             return -1;
         }
         int esquerda = Altura(no.esquerda);
         int direita = Altura(no.direita);
         if (esquerda > direita)
-            return 1 + esquerda;
+            return 1+ esquerda;
         return 1 + direita;
     }
-
-    public Node RotDireita(Node no) {
-        Node novaRaiz = no.esquerda;
-        Node temp = novaRaiz.direita;
-        novaRaiz.direita = no;
-        no.esquerda = temp;
-        return novaRaiz;
-    }
-
-    public Node RotEsquerda(Node no) {
-        Node novaRaiz = no.direita;
+    
+    public Node RotDireita(Node no){
+        Node novaRaiz = raiz.esquerda;
         Node temp = novaRaiz.esquerda;
-        novaRaiz.esquerda = no;
-        no.direita = temp;
+        novaRaiz.direita = raiz;
+        raiz.esquerda = temp;
         return novaRaiz;
-    }
-
-    public void imprimirArvore(Node no) {
-        if (no == null)
-            return;
-        imprimirArvore(no.esquerda);
-        System.out.println(no.info);
-        imprimirArvore(no.direita);
-    }
-
-    public Graph gerarGrafo(Node no) {
-        Graph g = new SingleGraph("Arvore");
-        g.setAutoCreate(true);
-        g.setStrict(false);
-
-        Node n = g.addNode(no.info);
-        g.addAttribute("ui.label", no.info);
-
-        if (no.esquerda != null) {
-            Node nE = gerarGrafo(no.esquerda);
-            g.addEdge("E", n, nE);
         }
-
-        if (no.direita != null) {
-            Node nD = gerarGrafo(no.direita);
-            g.addEdge("D", n, nD);
-        }
-
-        return g;
-    }
-
-    public static void main(String[] args) {
-        arvore ar = new arvore();
-
-        ar.Adicionar(65);
-        ar.Adicionar(95);
-        ar.A```
-    }
+    public Node RotEsquerda(Node no){
+        Node novaRaiz = raiz.direita;
+        Node temp = novaRaiz.esquerda;
+        novaRaiz.esquerda = raiz;
+        raiz.direita = temp;
+        return novaRaiz;
+        }        
 }
+
