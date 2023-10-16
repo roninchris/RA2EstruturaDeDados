33193ee33dd3c4237066992dc4cebbc659cb8ad3
Chris <102271783+roninchris@users.noreply.github.com>
2023-10-15 21:29:53 -0300

    Update main.java

--- main.java
+++ main.java
@@ -13,41 +13,57 @@ public class Main {
             ArvoreBinaria arvoreBST = new ArvoreBinaria();
             Random random = new Random();
 
+            // Inserção
             inicio = System.currentTimeMillis();
             for (int i = 0; i < tamanho; i++) {
                 arvoreBST.inserir(random.nextInt(100000));
             }
+            fim = System.currentTimeMillis();
+            System.out.println("Árvore Binária (BST) - Inserção: " + (fim - inicio) + " ms");
 
+            // Busca
+            inicio = System.currentTimeMillis();
             for (int i = 0; i < tamanho; i++) {
                 arvoreBST.buscar(random.nextInt(100000));
             }
+            fim = System.currentTimeMillis();
+            System.out.println("Árvore Binária (BST) - Busca: " + (fim - inicio) + " ms");
 
+            // Remoção
+            inicio = System.currentTimeMillis();
             for (int i = 0; i < tamanho; i++) {
                 arvoreBST.remover(random.nextInt(100000));
             }
-
             fim = System.currentTimeMillis();
-            System.out.println("Árvore Binária (BST) - Tempo total: " + (fim - inicio) + " ms");
+            System.out.println("Árvore Binária (BST) - Remoção: " + (fim - inicio) + " ms");
 
             // Árvore AVL
             ArvoreAVL arvoreAVL = new ArvoreAVL();
             random = new Random();
 
+            // Inserção
             inicio = System.currentTimeMillis();
             for (int i = 0; i < tamanho; i++) {
                 arvoreAVL.inserir(random.nextInt(100000));
             }
+            fim = System.currentTimeMillis();
+            System.out.println("Árvore AVL - Inserção: " + (fim - inicio) + " ms");
 
+            // Busca
+            inicio = System.currentTimeMillis();
             for (int i = 0; i < tamanho; i++) {
                 arvoreAVL.buscar(random.nextInt(100000));
             }
+            fim = System.currentTimeMillis();
+            System.out.println("Árvore AVL - Busca: " + (fim - inicio) + " ms");
 
+            // Remoção
+            inicio = System.currentTimeMillis();
             for (int i = 0; i < tamanho; i++) {
                 arvoreAVL.remover(random.nextInt(100000));
             }
-
             fim = System.currentTimeMillis();
-            System.out.println("Árvore AVL - Tempo total: " + (fim - inicio) + " ms");
+            System.out.println("Árvore AVL - Remoção: " + (fim - inicio) + " ms");
 
             System.out.println();
         }
